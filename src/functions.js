6b3c9eda130035a8922efd1689c90a5519e07f75
MR-Elbarbary <elbarbary7m@gmail.com>
2024-09-21 19:34:40 +0300

    Add functions with passed tests part(2 / 2)

--- src/functions.js
+++ src/functions.js
@@ -26,3 +26,40 @@ export class Calculator {
         return num1 * num2
     }
 }
+
+export function caesarCipher(str, shift) {
+  var output = "";
+  for (var i = 0; i < str.length; i++) {
+    var ascii = str[i].charCodeAt();
+    if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) {
+      ascii += shift;
+      if ((ascii > 90 && str[i] <= 'Z') || ascii > 122) {
+        ascii -= 26;
+      }
+    }
+    output += String.fromCharCode(ascii);
+  }
+  return output;
+}
+
+export function analyzeArray(arr){
+  let len = arr.length;
+  let min = arr[len-1];
+  let max = arr[len-1];
+  let avr = 0;
+  while (arr.length !== 0) {
+    let num = arr.pop();
+    if (num > max) {
+      max = num;
+    } else if ( num < min) {
+      min = num;
+    }
+    avr = avr + num;
+  }
+  avr = avr / len;
+  return {
+    average: avr,
+    min: min,
+    max: max,
+    length: len}
+}
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,4 +1,4 @@
-import { Calculator, capitalize, reverseString } from "./functions"
+import { analyzeArray, caesarCipher, Calculator, capitalize, reverseString } from "./functions"
 
 test('capitalize', () => {
     expect(capitalize('hello')).toBe('Hello');
@@ -15,4 +15,20 @@ test('calculator', ( ) =>{
     expect(calculator.subtract(40, 20)).toBe(20);
     expect(calculator.divide(1,3)).toBeCloseTo(0.33333);
     expect(calculator.multiply(10, 80)).toBe(800);
+});
+
+test('caesarCipher', () =>{
+    expect(caesarCipher('hello world', 2)).toBe('jgnnq yqtnf'),
+    expect(caesarCipher('HELLO WORLD', 2)).toBe('JGNNQ YQTNF'),
+    expect(caesarCipher('XYZ', 2)).toBe('ZAB')
+    expect(caesarCipher('xyz', 2)).toBe('zab')
+});
+
+test('analyzeArray', () =>{
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+     })
 });
